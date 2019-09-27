0e461d7bec13e9ac00629ba18be9c877a121c4c3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-27 14:26:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School
+
+  def initialize(name)
+  @school = name
+  @roster = {}
+  end
+ 
+  def add_student(name, grade)
+  roster[grade] = []
+  roster[grade] << name
+  roster
+  end
+
+  def roster=(add_student)
+    @roster = add_student
+  end
+  
+  def roster
+    @roster
+  end
+
+  def grade(num)
+if num == roster[:grade]
+roster[:name]
+end
+  end
+
+  def sort
+roster.sort
+  end
+
+end
+
+school = School.new("Bayside High School")
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
